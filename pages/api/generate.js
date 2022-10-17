fcf556e409feed04c13be41208e1f7f71367b9a3
Nipuni35 <nipunitharu35@gmail.com>
2022-10-17 17:42:18 -0600

    added changes to generate and index js files to retreive sql for a text

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -11,7 +11,7 @@ export default async function (req, res) {
             model: "code-davinci-002",
             prompt: generatePrompt(req.body.query),
             temperature: 0,
-            max_tokens: 15,
+            max_tokens: 150,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
